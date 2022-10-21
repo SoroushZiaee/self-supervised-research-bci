77c511db695c9ff74ad0a0a7df601377f8d2e533
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2022-10-21 16:01:41 +0330

    modify prepare dataset

--- scripts/tools/prepare_feature.py
+++ scripts/tools/prepare_feature.py
@@ -1,9 +1,27 @@
 from pase_eeg.lit_modules.pase_lit import PASE
 
+import argparse
+import os
+import pandas as pd
 
-def main():
-    print("Everything Works fine")
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    # For specifying on WANDB
+    parser.add_argument("--data_path", type=str)
+
+    return parser.parse_args()
+
+
+def run(data_path: str):
+    meta_data = pd.read_csv(os.path.join())
+
+
+def main(conf):
+    data_path = conf["data_path"]
+    run(data_path)
 
 
 if __name__ == "__main__":
-    main()
+    conf = vars(parse_args())
+    main(conf)
