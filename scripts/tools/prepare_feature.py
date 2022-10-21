12f1efd757b01546d1f9725199a36447b92c9a84
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2022-10-21 16:32:54 +0330

    configure model

--- scripts/tools/prepare_feature.py
+++ scripts/tools/prepare_feature.py
@@ -14,15 +14,30 @@ def parse_args():
     # For specifying on WANDB
     parser.add_argument("--data_path", type=str)
     parser.add_argument("--electrode_path", type=str)
+    parser.add_argument("--weight_path", type=str)
+    parser.add_argument("--emb_dim", type=int)
 
     return parser.parse_args()
 
 
 def prepare_dataset(data_path: str, eeg_electrode_positions, transforms):
-    return BCI2aDataset(eeg_electrode_positions, data_path)
+    dataset = BCI2aDataset(eeg_electrode_positions, data_path)
+    dataset.make_flatten()
+
+    return dataset
 
 
-def run(data_path: str, electrode_path: str):
+def prepare_model(electrode_path, emb_dim: int, pretrained_backend_weights_path: str):
+    pase = PASE(
+        electrode_path,
+        emb_dim,
+        pretrained_backend_weights_path=pretrained_backend_weights_path,
+    )
+
+    return pase
+
+
+def run(data_path: str, electrode_path: str, weight_path: str, emb_dim: int):
     (
         eeg_electrode_positions,
         eeg_electrods_plane_shape,
@@ -31,17 +46,17 @@ def run(data_path: str, electrode_path: str):
     transforms = Compose([ToTensor(device="cuda:0")])
 
     dataset = prepare_dataset(data_path, eeg_electrode_positions, transforms)
-    dataset.make_flatten()
+    model = prepare_model(electrode_path, emb_dim, weight_path)
 
-    wav, label = dataset[0]
-
-    print(wav.keys())
+    print(model)
 
 
 def main(conf):
     data_path = conf["data_path"]
     electrode_path = conf["electrode_path"]
-    run(data_path, electrode_path)
+    weight_path = conf["weight_path"]
+    emb_dim = conf["emb_dim"]
+    run(data_path, electrode_path, weight_path, emb_dim)
 
 
 if __name__ == "__main__":
