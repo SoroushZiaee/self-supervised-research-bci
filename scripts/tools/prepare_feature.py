9ace0a25336ca77331e912a1734e2daaa89455eb
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2022-10-21 23:15:22 +0330

    print types of labels

--- scripts/tools/prepare_feature.py
+++ scripts/tools/prepare_feature.py
@@ -85,6 +85,8 @@ def run(
 
     embeddings = model.forward(wav)
 
+    print(f"type : {type(label)}")
+    print(f"label : {label['label']}")
     is_saved = save_features(embeddings, label["label"], output_path)
 
     if is_saved:
