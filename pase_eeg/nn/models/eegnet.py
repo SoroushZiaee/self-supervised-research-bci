b35be96db1ceb9b3f27c09886d9319d6fa2f7dd7
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2022-10-22 01:37:58 +0330

    perform on patient 1

--- pase_eeg/nn/models/eegnet.py
+++ pase_eeg/nn/models/eegnet.py
@@ -145,8 +145,6 @@ class EEGNetv2Emb(nn.Module):
 
     def forward(self, x, device=None):
         x = self._forward_emb(x)
-        print(f"x shape : {x.size()}")
-
         x = self.fc1(x)
         return x
 
